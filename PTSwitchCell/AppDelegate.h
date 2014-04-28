b5835c6089f023b9e4e41f1a1862ea019c017a0f
Phillip Harris <phillip.harris@atlanticbt.com>
2014-04-28 13:12:04 -0400

    Initial Commit

---
+++ PTSwitchCell/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PTSwitchCell
+//
+//  Created by Phillip Harris on 4/28/14.
+//  Copyright (c) 2014 Phillip Harris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
